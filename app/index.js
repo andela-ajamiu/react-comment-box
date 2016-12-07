e053f1fa21a8a8aee0c6fd84114f5337a74f1e85
Emjay <abdulmujeeb.jamiu@andela.com>
2016-12-07 16:52:34 +0100

    add Comment and CommentForm Components to CommentBox

--- app/index.js
+++ app/index.js
@@ -5,27 +5,27 @@ import ReactDOM from 'react-dom';
 class CommentBox extends React.Component {
   constructor() {
     super();
-    this.state = {showComments: false};
+    this.state = {showComments: false,
+                  commentList: [
+                    {id: 1, author: "Ben Rub", body: "just wanna comment and hope it shows...", avatarUrl: "http://www.avatar-zone.com/Avatars/Fun/Avatar-25.jpg" },
+                    {id: 2, author: "Bill Gates", body: "and just another comment in no time", avatarUrl: "http://www.avatar-zone.com/Avatars/Fun/Avatar-25.jpg" },
+                    {id: 3, author: "Ben Rub", body: "just wanna comment and hope it shows...", avatarUrl: "http://www.avatar-zone.com/Avatars/Fun/Avatar-25.jpg" },
+                    {id: 4, author: "Bill Gates", body: "and just another comment in no time", avatarUrl: "http://www.avatar-zone.com/Avatars/Fun/Avatar-25.jpg" },
+                    {id: 5, author: "Ben Rub", body: "just wanna comment and hope it shows...", avatarUrl: "http://www.avatar-zone.com/Avatars/Fun/Avatar-25.jpg" },
+                    {id: 6, author: "Bill Gates", body: "and just another comment in no time", avatarUrl: "http://www.avatar-zone.com/Avatars/Fun/Avatar-25.jpg" },
+                    {id: 7, author: "Ben Rub", body: "just wanna comment and hope it shows...", avatarUrl: "http://www.avatar-zone.com/Avatars/Fun/Avatar-25.jpg" },
+                    {id: 8, author: "Bill Gates", body: "and just another comment in no time", avatarUrl: "http://www.avatar-zone.com/Avatars/Fun/Avatar-25.jpg" },
+                    {id: 9, author: "Ben Rub", body: "just wanna comment and hope it shows...", avatarUrl: "http://www.avatar-zone.com/Avatars/Fun/Avatar-25.jpg" },
+                    {id: 10, author: "Bill Gates", body: "and just another comment in no time", avatarUrl: "http://www.avatar-zone.com/Avatars/Fun/Avatar-25.jpg" },
+                    {id: 11, author: "Ben Rub", body: "just wanna comment and hope it shows...", avatarUrl: "http://www.avatar-zone.com/Avatars/Fun/Avatar-25.jpg" },
+                    {id: 12, author: "Bill Gates", body: "and just another comment in no time", avatarUrl: "http://www.avatar-zone.com/Avatars/Fun/Avatar-25.jpg" }
+                  ]
+                 };
   };
 
   _getComments() {
-    const commentList = [
-      {id: 1, author: "Ben Rub", body: "just wanna comment and hope it shows...", avatarUrl: "http://www.avatar-zone.com/Avatars/Fun/Avatar-25.jpg" },
-      {id: 2, author: "Bill Gates", body: "and just another comment in no time", avatarUrl: "http://www.avatar-zone.com/Avatars/Fun/Avatar-25.jpg" },
-      {id: 3, author: "Ben Rub", body: "just wanna comment and hope it shows...", avatarUrl: "http://www.avatar-zone.com/Avatars/Fun/Avatar-25.jpg" },
-      {id: 4, author: "Bill Gates", body: "and just another comment in no time", avatarUrl: "http://www.avatar-zone.com/Avatars/Fun/Avatar-25.jpg" },
-      {id: 5, author: "Ben Rub", body: "just wanna comment and hope it shows...", avatarUrl: "http://www.avatar-zone.com/Avatars/Fun/Avatar-25.jpg" },
-      {id: 6, author: "Bill Gates", body: "and just another comment in no time", avatarUrl: "http://www.avatar-zone.com/Avatars/Fun/Avatar-25.jpg" },
-      {id: 7, author: "Ben Rub", body: "just wanna comment and hope it shows...", avatarUrl: "http://www.avatar-zone.com/Avatars/Fun/Avatar-25.jpg" },
-      {id: 8, author: "Bill Gates", body: "and just another comment in no time", avatarUrl: "http://www.avatar-zone.com/Avatars/Fun/Avatar-25.jpg" },
-      {id: 9, author: "Ben Rub", body: "just wanna comment and hope it shows...", avatarUrl: "http://www.avatar-zone.com/Avatars/Fun/Avatar-25.jpg" },
-      {id: 10, author: "Bill Gates", body: "and just another comment in no time", avatarUrl: "http://www.avatar-zone.com/Avatars/Fun/Avatar-25.jpg" },
-      {id: 11, author: "Ben Rub", body: "just wanna comment and hope it shows...", avatarUrl: "http://www.avatar-zone.com/Avatars/Fun/Avatar-25.jpg" },
-      {id: 12, author: "Bill Gates", body: "and just another comment in no time", avatarUrl: "http://www.avatar-zone.com/Avatars/Fun/Avatar-25.jpg" }
-    ];
-
     return(
-      commentList.map((comment) => 
+      this.state.commentList.map((comment) => 
         <Comment key={comment.id} author={comment.author} body={comment.body} avatar={comment.avatarUrl} />
     ));
   };
@@ -64,6 +64,11 @@ class CommentBox extends React.Component {
     };
   };
 
+  _addComment(comment) {
+    comment.id = this.state.commentList.length + 1;
+    this.setState({commentList: this.state.commentList.concat(comment)});
+  };
+
   render() {
     const comments = this._getComments();
 
@@ -75,6 +80,7 @@ class CommentBox extends React.Component {
 
     return(
       <div className="comment-box">
+        <CommentForm handleCreate={this._addComment.bind(this)} />
         {this._getPopularPost(comments.length)}
         <h3>Comments</h3>
         <button onClick={this._toggleCommentsDisplay.bind(this)}>{this._getButtonText()}</button>
@@ -87,17 +93,59 @@ class CommentBox extends React.Component {
 };
 
 
-class Comment extends React.Component {
+class CommentForm extends React.Component {
+  _submitComment() {
+    let comment = {
+      author: this.refs.author.value,
+      body: this.refs.body.value
+    };
+
+    this.props.handleCreate(comment);
+
+    this.refs.author.value = "";
+    this.refs.body.value = "";
+  };
+
   render() {
+    return(
+      <form>
+        <input type="text" placeholder="Author" ref="author" />
+        <input type="text" placeholder="Body" ref="body" />
+        <button onClick={this._submitComment.bind(this)}>Add Comment</button>
+      </form>
+    );
+  };
+};
+
+
+class Comment extends React.Component {
+  constructor() {
+    super();
+    this.state = {isAbusive: false};
+  };
+
+  _toggleAbusive() {
+    this.setState({isAbusive: !this.state.isAbusive});
+  };
+
+  render() {
+    let commentNode;
+
+    if (this.state.isAbusive == false) {
+      commentNode = this.props.body;
+    } else {
+      commentNode = "This comment is marked as ABUSIVE";
+    };
+
     return(
       <div className="comment">
         <img src={this.props.avatar} alt={`${this.props.author}'s picture`} />
         <p className="comment-header">{this.props.author}</p>
-        <p className="comment-body">{this.props.body}</p>
+        <p className="comment-body">{commentNode}</p>
         <div className="comment-footer">
-          <a href="#" className="comment-footer-delete">
-            Delete Comment
-          </a>
+          <a href="#" className="comment-footer-delete">DELETE COMMENT</a>
+          <br/>
+          <a href="#" className="comment-footer-report" onClick={this._toggleAbusive.bind(this)}>REPORT AS ABUSE</a>
         </div>
         <br/><br/>
       </div>
